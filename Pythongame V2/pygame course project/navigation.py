fcf2f4325b69d876d3a479b971beb70ac3ee83a2
nikiyyy <44340403+nikiyyy@users.noreply.github.com>
2020-03-03 00:02:59 +0200

    Delete navigation.py

--- Pythongame V2/pygame course project/navigation.py
+++
@@ -1,9 +0,0 @@
-import combat as comb
-import Player as PL  
-
-
-player=PL.Player("human",7,880)
-#comb.Combat(player,5,"Thug",emem2type="Bandit",emem3type="Pit_Fighter",emem4type="Pit_Fighter",emem5type="Pit_Fighter")             
-comb.Combat(player,2,"Thug",enemNam1="george",emem2type="Bandit")     
-#comb.Combat(player,1,"Thug",enemNam1="bob") 
-print("end game")   
